7286791c7a6eb1f915ca8f95a8c755ccfa2e90c3
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-03-12 10:41:43 +0800

    Upon user sign up, will receive email to confirm email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable 
 
   # define roles
-  enum role: [:user, :trader, :admin]
+  enum role: [:trader, :admin]
+
   # after creating a new account, set account to default role
   after_initialize :set_default_role, :if => :new_record?
+
   def set_default_role
-    self.role ||= :user
+    self.role ||= :trader
+  end
+
+  def admin? 
+    self.role == 'admin'
+  end
+
+  def trader? 
+    self.role == 'trader'
   end
 end 
