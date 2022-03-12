55b633017f906d504ef3ac70f4460d683c877a84
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-03-12 11:06:37 +0800

    uncomment for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,8 +122,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
