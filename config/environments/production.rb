0a6b806e62f77322617b0f72e18441a12404d2e3
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-03-12 15:41:24 +0800

    Prod mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,21 +119,17 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  
-  # change to true to allow email to be sent during development
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :mailjet_api
+  # config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "mail.google.com", ####important
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.credentials.prod_mailer_email,
-    password: Rails.application.credentials.prod_mailer_password
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "mail.google.com", ####important
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: Rails.application.credentials.prod_mailer_email,
+  #   password: Rails.application.credentials.prod_mailer_password
+  # }
   
 end
