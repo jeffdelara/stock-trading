fa8a99bcdb374171ff82e7d6fe3fb80100123191
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-03-17 17:39:55 +0800

    Mailjet mailer settings for action mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,19 +2,9 @@ class UserMailer < ApplicationMailer
   default from: 'groupjeffals@gmail.com'
 
   def welcome_email 
-    # delivery_options = {
-    #   :api_key    => Rails.application.credentials.mailjet_api_key,
-    #   :secret_key => Rails.application.credentials.mailjet_secret_key, 
-    #   :version    => 'v3.1'
-    # }
-
     @user = params[:user]
     
-    mail(
-      :to => @user.email, 
-      :subject => 'Account Approved!'
-      # :delivery_method_options => delivery_options
-    )
+    mail :to => @user.email, :subject => 'Account Approved!'
   end
   
 end
