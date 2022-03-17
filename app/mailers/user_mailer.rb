161a090717e376ed8c6c460266e3bdea24221bef
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-03-17 17:27:54 +0800

    Explicit mailjet send mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,19 @@
 class UserMailer < ApplicationMailer 
 
   def welcome_email 
+    delivery_options = {
+      :api_key    => Rails.application.credentials.mailjet_api_key,
+      :secret_key => Rails.application.credentials.mailjet_secret_key, 
+      :version    => 'v3.1'
+    }
+
     @user = params[:user]
-    mail :to => @user.email, :subject => 'Account Approved!'
+    
+    mail(
+      :to => @user.email, 
+      :subject => 'Account Approved!', 
+      :delivery_method_options => delivery_options
+    )
   end
   
 end
