4d0e4da762110498c169df0a539c23480538f85b
Jeffrey de Lara <jeffreytdelara@gmail.com>
2022-03-17 16:43:31 +0800

    Deliver now

--- app/controllers/admin/pending_traders_controller.rb
+++ app/controllers/admin/pending_traders_controller.rb
@@ -8,7 +8,7 @@ class Admin::PendingTradersController < ApplicationController
     @user = User.find params[:id]
     
     if @user.update(:approved => true)
-      UserMailer.with(user: @user).welcome_email.deliver_now!
+      UserMailer.with(user: @user).welcome_email.deliver_now
       redirect_to admin_pending_traders_path, :notice => 'User approved.'
     else
       render :index
