c0db19e74df834a8aaa8bab3289d88e827acc3ee
Alsace Cablar <cablaralsace@gmail.com>
2022-03-25 20:17:59 +0800

    sell stocks feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   resources :quotes, :except => [:show]
   get 'quotes/:symbol', to: 'quotes#show'
+  get 'sell_stocks/:symbol', to: 'sell_stocks#show', as: 'get_sellstock'
+  post 'sell_stocks/:symbol', to: 'sell_stocks#create', as: 'sell_stock'
   get 'portfolio', to: 'stocks#index'
   resources :stocks
 end
